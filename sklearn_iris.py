068e36434e9a662bf5885f49a473463eaff249e2
Yi Gao <yigao1983@gmail.com>
2016-11-07 21:42:49 -0600

    Update sklearn_iris.py

--- sklearn_iris.py
+++ sklearn_iris.py
@@ -1,8 +1,38 @@
 import sklearn.datasets as datasets
 import sklearn.model_selection as ms
 import sklearn.preprocessing as pp
+import sklearn.linear_model as lm
+import sklearn.metrics as metrics
 import numpy as np
+import matplotlib.colors as clrs
+import matplotlib.pyplot as plt
 
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+    
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    
+    cmap = clrs.ListedColormap(colors[:len(np.unique(y))])
+    
+    x1_min, x1_max = X[:,0].min() - 1, X[:,0].max() + 1
+    x2_min, x2_max = X[:,1].min() - 1, X[:,1].max() + 1
+    
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), \
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T).reshape(xx1.shape)
+    
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+    
+    # plot all samples
+    X_test, y_test = X[test_idx, :], y[test_idx, :]
+    
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y==cl,0], y=X[y==cl,1], alpha=0.8, \
+                    c=cmap(idx), marker=markers[idx], label=cl)
+    
 def main():
     
     iris = datasets.load_iris()
@@ -15,6 +45,21 @@ def main():
     sc = pp.StandardScaler().fit(X_train)
     X_train_std = sc.transform(X_train)
     X_test_std = sc.transform(X_test)
+    
+    ppn = lm.Perceptron(n_iter=40, eta0=0.01, random_state=0).fit(X_train_std, y_train)
+    y_pred = ppn.predict(X_test_std)
+    
+    print('Misclassified samples: {}'.format((y_test != y_pred).sum()))
+    print('Accuracy: {:10.2f}'.format(metrics.accuracy_score(y_test, y_pred)))
+    
+    X_combined_std = np.vstack((X_train_std, X_test_std))
+    y_combined = np.hstack((y_train, y_test))
+    
+    plt.figure()
+    plot_decision_regions(X_test_std, y_test, ppn)
+    plt.xlabel('petal length (standardized)')
+    plt.ylabel('petal width (standardized)')
+    plt.show()
 
 if __name__ == "__main__":
     
