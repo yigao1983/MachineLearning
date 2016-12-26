68eef9797ee592bea7d361d9aebbe62e9d0958a7
Yi Gao <yigao1983@gmail.com>
2016-12-26 14:38:34 -0600

    Update sklearn_wine.py

--- sklearn_wine.py
+++ sklearn_wine.py
@@ -1,12 +1,42 @@
 import numpy as np
 import pandas as pd
+import sklearn.linear_model as lm
 import sklearn.preprocessing as pp
+import sklearn.decomposition as dc
 import sklearn.model_selection as ms
-import sklearn.neighbors as nb
-import sklearn.ensemble as es
 import matplotlib.pyplot as plt
+import matplotlib.colors as clrs
 import SBS
 
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+    
+    markers = ("s", "x", "^", "v")
+    colors = ("red", "blue", "lightgreen", "gray", "cyan")
+    cmap = clrs.ListedColormap(colors[:len(np.unique(y))])
+    
+    x1_min, x1_max = X[:,0].min() - 1, X[:,0].max() + 1
+    x2_min, x2_max = X[:,1].min() - 1, X[:,1].max() + 1
+    
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), \
+                           np.arange(x2_min, x2_max, resolution))
+    
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    
+    plt.figure()
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+    
+    for idx, cl in enumerate(np.unique(y)):
+        
+        plt.scatter(x=X[y==cl,0], y=X[y==cl,1], alpha=0.8, c=cmap(idx), marker=markers[idx], label=cl)
+        
+    plt.xlabel("PC 1")
+    plt.ylabel("PC 2")
+    plt.legend(loc="best")
+    plt.show()
+    
 def main():
     
     df_wine = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/wine/"
@@ -25,6 +55,49 @@ def main():
     X_train_std = stdsc.fit_transform(X_train)
     X_test_std = stdsc.transform(X_test)
     
+    pca = dc.PCA(n_components=2)
+    lr = lm.LogisticRegression()
+    
+    X_train_pca = pca.fit_transform(X_train_std)
+    X_test_pca = pca.transform(X_test_std)
+    """
+    colors = ['r', 'b', 'g']
+    markers = ['s', 'x', 'o']
+    plt.figure()
+    for l, c, m in zip(np.unique(y_train), colors, markers):
+        plt.scatter(X_train_pca[y_train==l, 0], X_train_pca[y_train==l, 1], c=c, label=l, marker=m)
+    plt.xlabel("PC 1")
+    plt.ylabel("PC 2")
+    plt.legend("best")
+    plt.show()
+    """
+    lr.fit(X_train_pca, y_train)
+    
+    plot_decision_regions(X_train_pca, y_train, classifier=lr)
+    
+    plot_decision_regions(X_test_pca, y_test, classifier=lr)
+    """
+    plt.figure()
+    plt.bar(range(1, 14), var_exp, alpha=0.5, align="center", label="individual explained variance")
+    plt.step(range(1, 14), cum_var_exp, where="mid", label="cumulative explained variance")
+    plt.xlabel("Principal components")
+    plt.ylabel("Explained variance ratio")
+    plt.legend(loc="best")
+    plt.show()
+    
+    eigen_pairs = [(np.abs(eigen_vals[i]), eigen_vecs[:,i]) for i in range(len(eigen_vals))]
+    eigen_pairs.sort(reverse=True)
+    
+    w = np.vstack((eigen_pairs[0][1], eigen_pairs[1][1])).T
+    
+    X_train_pca = X_train_std.dot(w)
+    
+    colors = ['r', 'b', 'g']
+    markers = ['s', 'x', 'o']
+    
+    """
+    
+    """
     knn = nb.KNeighborsClassifier(n_neighbors=2)
     sbs = SBS.SBS(estimator=knn, k_features=1).fit(X_train_std, y_train)
     
@@ -53,7 +126,8 @@ def main():
     plt.xlim([-1, X_train.shape[1]])
     plt.tight_layout()
     plt.show()
-    
+    """
 if __name__ == "__main__":
     
     main()
+    
